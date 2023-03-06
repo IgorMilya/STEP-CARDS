41620f7eca07fc6e0816db9714411bf05d212dc4
Banzay700 <lotkoolexandr@gmail.com>
2023-03-06 18:33:14 +0200

    add button-component and basic settings

--- src/js/UI/Button.js
+++ src/js/UI/Button.js
@@ -1,5 +1,8 @@
-export const Button = () => {
+export const Button = (selector, text) => {
   const Button = document.createElement('button')
- 
+  Button.className = 'button'
+  Button.classList.add(selector)
+  Button.textContent = text
+
   return Button
 }
