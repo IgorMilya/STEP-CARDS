551d786da1369e318b78843f2a38607ee176b9f4
Ivan Basiuk <vaanbass@gmail.com>
2023-03-12 13:47:28 +0000

    finish covid form

--- src/js/UI/Button.js
+++ src/js/UI/Button.js
@@ -15,3 +15,5 @@ export const headerLogInButton = Button('header-login', 'Log In')
 export const cancelButton = Button('modal-visit__button-cancel', 'Cancel')
 
 export const saveButton = Button('modal-visit__button-save', 'Save')
+
+export const covidButton = Button('covid-button-show', 'Show world')
---
+++ src/js/components/Sidebar/sidebarInfo.js
@@ -0,0 +1,38 @@
+import { doc } from 'prettier'
+import { covidButton } from '../../UI/Button'
+import { covidData } from '../Registration'
+import iconUA from '../../../assets/images/covid/ukraine.png'
+
+const { UAnewConfirmed, UAallConfirmed, UAallDeaths } = covidData
+
+export const sidebarInfo = () => {
+  const sidebarInfo = document.createElement('div')
+  sidebarInfo.classList.add('sidebar-info')
+
+  sidebarInfo.innerHTML = `
+     
+     <div class="sidebar-covid">
+     <div class="covid-body">
+     <div class="covid-title">Covid information</div>
+      <div class="covid-icon">
+      <img class="covid-icon-img" src = '${iconUA}'>
+      <div class="covid-icon-title">Ukraine</div>
+      </div>
+      
+     <div class="covid-info">
+     <div class="covid-new-recoverdet">Today indected - ${UAnewConfirmed}</div>
+     <div class="covid-infected">All infected - ${UAallConfirmed}</div>
+     <div class="covid-deaths">All deaths - ${UAallDeaths}</div>
+     <div class="covid-button">${covidButton}</div>
+     </div>
+     </div>
+     </div>
+     
+     <div class="sidebar-footer">
+     <div class="sidebar-footer-title"> &copy; HandyHealth 2023</div>
+     <div class="sidebar-footer-text">HandyHealth is medical management for all doctors, patient & Staff</div>
+     </div>
+     </div>`
+
+  return sidebarInfo
+}
