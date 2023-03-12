50a58746738a229c3e6f29b74cb90c01fe99a0a0
IvanBasss <122455382+IvanBasss@users.noreply.github.com>
2023-03-12 15:24:45 +0200

    finish covid

--- src/js/layouts/Sidebar.js
+++ src/js/layouts/Sidebar.js
@@ -1,54 +1,16 @@
-import covidWord from '../../assets/images/covid/global.png'
-import { asideList, asideListSupport } from './layoutUtils'
+import { sidebarInfo } from '../components/Sidebar/sidebarInfo'
+import { sidebarMenu } from '../components/Sidebar/sidebarMenu'
 
 export const Sidebar = () => {
   const Sidebar = document.createElement('div')
-  Sidebar.classList.add('sidebar')
+  const sidebarContent = document.createElement('div')
 
-  Sidebar.innerHTML = `
-  <div class="sidebar-content">
-  <div class="sidebar-menu">
-  <div class="menu-title">Managment</div>
-    <ul class="sidebar-list">
-    ${asideList
-      .map(
-        ({ name, path }) =>
-          ` <li class="sidebar-item">${path} <a href="#" class="aside-link">${name}</a>
-        </li>
-        `
-      )
-      .join('')}</ul>
-      <div class="support-line"></div>
-      <div class="menu-support">Supports</div>
-      <ul class="sidebar-list">
-        ${asideListSupport
-          .map(
-            ({ name, path }) =>
-              `<li class="sidebar-item">${path}<a href="#" class="aside-link">${name}</a></li>`
-          )
-          .join('')}
-</ul>
-  </div>
-  <div class="sidebar-covid">
-  <div class="covid-body">
-  <div class="covid-title">Covid information</div>
-  <div class="covid-icon">
-  <img src="${covidWord}"/> world
-  </div>
-  <div class="covid-info">
-  <div class="covid-infected">123213</div>
-  <div class="covid-recoverdet">43214321</div>
-  <div class="covid-deaths">54325</div>
-  <div class="covid-button">Covid button</div>
-  </div>
-  </div>
-  </div>
-  <div class="sidebar-footer">
-  <div class="sidebar-footer-title"> &copy; HandyHealth 2023</div>
-  <div class="sidebar-footer-text">HandyHealth is medical management for all doctors, patient & Staff</div>
-  </div>
-</div>
-`
+  Sidebar.classList.add('sidebar')
+  sidebarContent.classList.add('sidebar-content')
+
+  Sidebar.append(sidebarContent)
+  sidebarContent.append(sidebarMenu())
+  sidebarContent.append(sidebarInfo())
 
   return Sidebar
 }
--- src/js/modules/renderLoginPage.js
+++ src/js/modules/renderLoginPage.js
@@ -1,9 +1,12 @@
 import { Header, Main } from '../layouts'
 import { showLoginForm } from '../components/Registration/showLoginForm.js'
+import { changeValueCovid } from './changeValueCovid'
 
 export const renderLoginPage = () => {
   const app = document.getElementById('app')
 
   app.append(Header(), Main())
   showLoginForm()
+
+  changeValueCovid()
 }
