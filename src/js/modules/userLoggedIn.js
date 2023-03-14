0fb262889dd92c2893766169d8a7af5fa5cb06e8
imilay11 <imilay11@gmail.com>
2023-03-14 15:10:40 +0200

    Implemented search filter

--- src/js/modules/userLoggedIn.js
+++ src/js/modules/userLoggedIn.js
@@ -35,6 +35,33 @@ export const userLoggedIn = async () => {
   // ========================== //
 
   renderComponent(Dashboard)
+
+  ////////////////////////////////
+  const search = document.querySelector('.search')
+
+  search.oninput = () => {
+    const appointCart = document.querySelectorAll('.appointment')
+    const value = search.value.trim()
+
+    if (value !== '') {
+      appointCart.forEach(item => {
+        const visitorName = item.querySelector('.name')
+        const doctorName = item.querySelector('.doctor')
+
+        if (visitorName.innerText.search(value) === -1 && doctorName.innerText.search(value) === -1) {
+          item.style.display = 'none'
+        } else {
+          item.style.display = 'flex'
+        }
+      })
+    } else {
+      appointCart.forEach(item => {
+        item.style.display = 'flex'
+      })
+    }
+  }
+  //////////////////////
+
   // ВАЖЛИВА ХУЙНЯ
   const allAppointments = await getAllAppointments()
   allAppointments.forEach(item => {
