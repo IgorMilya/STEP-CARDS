f0d8d68c2874149bcee1e3b08cabd38e372613f1
Olexandr L <lotkoolexandr@gmail.com>
2023-03-15 10:20:53 +0000

    Bug fix/dashboard height

--- src/js/modules/getAllAppointments.js
+++ src/js/modules/getAllAppointments.js
@@ -1,12 +1,18 @@
-import { request } from '../tools'
-import { Appointment } from '../classes/index.js'
+import { find, request, create } from '../tools'
 import { setLocalData } from './localData.js'
+import noDataImage from '../../assets/images/dashboard/noData.svg'
 
 export const getAllAppointments = async () => {
   const token = localStorage.getItem('token')
   const allAppointments = await request({ url: '', method: 'GET', token })
+  const noDataImg = create('img', 'nodata-image')
+  const tableList = find('.table-list')
+  noDataImg.src = noDataImage
+
+  !allAppointments.length ? tableList.append(noDataImg) : ''
 
   allAppointments.forEach(obj => {
+
     const appointment = new Appointment(obj)
     appointment.addNewAppointment()
     setLocalData(obj)
