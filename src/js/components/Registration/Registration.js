947b70f7240ad22d49c86cbf27189933d7f8407e
Banzay700 <lotkoolexandr@gmail.com>
2023-03-17 14:11:22 +0200

    start works

--- src/js/components/Registration/Registration.js
+++ src/js/components/Registration/Registration.js
@@ -2,7 +2,7 @@ import { loginForm } from './index'
 import { create, createEl } from '../../tools'
 import regImg from '../../../assets/images/hero.svg'
 
-export const Registration = create('div', 'registration')
+export const Registration = createEl({ css: 'registration' })
 
 const registrationImg = createEl({ el: 'div', css: 'login-image' })
 const img = createEl({ el: 'img', src: regImg, alt: 'Registration Image' })
